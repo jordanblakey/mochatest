f3d17f26a36c71c9ed1ddcfd53d879faf3099b27
Jordan Blakey <poshski1@gmail.com>
2017-05-18 22:02:41 -0500

    Added organization using refactored function result variables and nested Mocha describe() methods

--- test/appTest.js
+++ test/appTest.js
@@ -1,22 +1,34 @@
 // const assert = require('assert');
 const assert = require('chai').assert;
-const sayHello = require('../app').sayHello;
-const addNumbers = require('../app').addNumbers;
-// const app = require('../app');
+// const sayHello = require('../app').sayHello;
+// const addNumbers = require('../app').addNumbers;
+const app = require('../app');
+
+// Results
+sayHelloResult = app.sayHello();
+addNumbersResult = app.addNumbers(5, 5);
 
 describe('App', function(){
-    it('App should return hello', function(){
-        let result = sayHello();
-        assert.equal(result, 'hello');
-    });
+    describe('sayHello()', function(){
+        it('App should return hello', function(){
+            // let result = app.sayHello();
+            assert.equal(sayHelloResult, 'hello');
+        });
 
-    it('sayHello should return type string', function(){
-        let result = sayHello();
-        assert.typeOf(result, 'string');
+        it('sayHello should return type string', function(){
+            // let result = app.sayHello();
+            assert.typeOf(sayHelloResult, 'string');
+        });
     });
+    describe('addNumbers()', function(){
+        it('addNumbers should be above 5', function(){
+            // let result = app.addNumbers(5,5);
+            assert.isAbove(addNumbersResult, 5);
+        })
 
-    it('addNumbers should be above 5', function(){
-        let result = addNumbers(5,5);
-        assert.isAbove(result, 5);
-    })
+        it('addNumbers should be a number', function(){
+            // let result = app.addNumbers(5,5);
+            assert.typeOf(addNumbersResult, 'number');
+        });
+    });
 });
